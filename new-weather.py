3fed64eaeff0d1318a0611117ec6d2c26b55c49e
Brandon Kraybill <bwkrayb@gmail.com>
2022-03-25 16:06:09 -0500

    smashrun integration works fully now. It will display information about the most recent run between 2pm and 5pm, and monthly status all other hours. This also opens up replacing the rest of the fields in that area with different data

--- new-weather.py
+++ new-weather.py
@@ -6,6 +6,7 @@ import urllib.request
 import json
 from libs.functions import get_icon, indent, indentThirds,  paste, write_weather, write_running_month, write_running_last, get_desc, imageIndent, imageIndentThirds
 from datetime import datetime,date
+from dateutil import parser
 from libs.waveshare_epd import epd7in5b_V2
 from PIL import Image, ImageDraw, ImageFont
 from settings import API_KEY,SMASHRUN_KEY
@@ -30,9 +31,9 @@ iconText = ImageFont.truetype(FONT, 80, index=0)
 bodyText = ImageFont.truetype(FONT, 20, index=0)
 timeText = ImageFont.truetype(FONT, 20, index=0)
 condText = ImageFont.truetype(FONT, 45, index=0)
-sunText = ImageFont.truetype(FONT,45,index=0)
-dateText = ImageFont.truetype(FONT,30,index=0)
-runText = ImageFont.truetype(FONT,45,index=0)
+sunText = ImageFont.truetype(FONT,50,index=0)
+dateText = ImageFont.truetype(FONT,35,index=0)
+runText = ImageFont.truetype(FONT,65,index=0)
 imageBlack = Image.new(mode='1', size=(w, h), color=255)
 drawBlack = ImageDraw.Draw(imageBlack)
 imageRed = Image.new(mode='1', size=(w, h), color=255)
@@ -81,8 +82,26 @@ def drawSmallTemp(hourText,temp,width,height):
     drawRed.text((indent(hourText,bodyText,w/4)+ width, height),hourText,font=bodyText,fill=0,align='left')
     drawBlack.text((indent(temp,iconText,w/4)+ width, height+15),temp,font=iconText,fill=0,align='left')
 
+def leftScreenPrint(textPrint,dataPrint,width,height):
+    drawRed.text((indent(textPrint,bodyText,w/2)+width,height),textPrint,font=bodyText,fill=0,align='left')
+    drawBlack.text((indent(dataPrint,sunText,w/2)+width,height+20),dataPrint, font=sunText, fill=0, align='left')
+
+def runDataPrint(dataPrint,width,height):
+    drawBlack.text((indent(dataPrint,runText,w/2)+width,height),dataPrint, font=runText, fill=0, align='left')
+
+def printVert(textPrint,startHeight,width):
+    i=0
+    changeHeight=0
+    while i < len(textPrint):
+        drawRed.text((indent(textPrint[i],bodyText,w)+width,startHeight+changeHeight),textPrint[i],font=bodyText,fill=0,align='left')
+        changeHeight+=bodyText.getsize(textPrint[i])[1]-5
+        i+=1
+
+
+#font.getsize(input)[1]
+
 try:
-#    write_weather()
+    write_weather()
     readLastRun()
     check_last_run()
     if lastRunID != lastRunJson[0]['activityId']:
@@ -159,44 +178,10 @@ try:
     else:
         curDesc1 = curDesc[0] 
 
-    #drawBlack.rectangle((400,240,800,480), fill=1, outline=0)
-    #drawRed.rectangle((300, 0, 600, 240), fill=1, outline=0)
 
     logo = get_icon(curID)
     logoLg = logo.resize((64,64))
     imageRed.paste(logoLg, (imageIndent(logo,w)-50,60))
-
-    totalDist = str(round((monthRunJson['totalDistance']*.621),2))
-
-    runCount = str(monthRunJson['runCount'])
-
-    if monthRunJson['longestRun'] == None:
-        longRun = '0'
-    else:
-        longRun = str(round((monthRunJson['longestRun'] * 0.621),2))
-
-    if monthRunJson['averageRunLength'] == None:
-        avgLen = '0'
-    else:
-        avgLen = str(round((monthRunJson['averageRunLength'] * 0.621),2))
-
-    datePrint = "Stats: " + monthText + " " + year
-
-    drawRed.text((indent(datePrint,dateText,w/2)+400, 160), datePrint, font=dateText, fill=0, align='left')
-
-    drawRed.text((indent("Total Dist",bodyText,w/4)+400,200),"Total Dist",font=bodyText,fill=0,align='left')
-    drawBlack.text((indent(totalDist + "mi",runText,w/4)+400, 220), totalDist + "mi", font=runText, fill=0, align='left')
-
-    drawRed.text((indent("Count",bodyText,w/4)+400,275),"Count",font=bodyText,fill=0,align='left')
-    drawBlack.text((indent(runCount + "runs",runText,w/4)+400, 295), runCount + "runs", font=runText, fill=0, align='left')
-
-    drawRed.text((indent("Longest",bodyText,w/4)+600,200),"Longest",font=bodyText,fill=0,align='left')
-    drawBlack.text((indent(longRun + "mi",runText,w/4)+600, 220), longRun + "mi", font=runText, fill=0, align='left')
-
-    drawRed.text((indent("Average",bodyText,w/4)+600,275),"Average",font=bodyText,fill=0,align='left')
-    drawBlack.text((indent(avgLen + "mi",runText,w/4)+600, 295), avgLen + "mi", font=runText, fill=0, align='left')
-    
-
 
 
 ###TEMP TEXT###
@@ -211,22 +196,76 @@ try:
 
 
     drawSmallTemp('Low',lowTemp,0,265)
-
     drawSmallTemp('High',highTemp,0,380)
+    drawSmallTemp(nextHourStr,nextHourTemp,170,265)
+    drawSmallTemp(nextHourStr2,nextHourTemp2,170,380)
 
-    drawSmallTemp(nextHourStr,nextHourTemp,200,265)
+    leftScreenPrint(sunriseText,sunriseFull,400,0)
+    leftScreenPrint(sunsetText,sunsetFull,400,80)
 
-    drawSmallTemp(nextHourStr2,nextHourTemp2,200,380)
+    if 14 <= int(dt.strftime('%H')) < 17:
+        time = parser.parse(lastRunJson[0]['startDateTimeLocal'])
+        timeStr = time.strftime("%x")
+        today = date.today().strftime("%x")
+        if timeStr == today:
+            runDate = "Today"
+        else:
+            runDate = timeStr
+
+        drawRed.text((indent("Last",dateText,w)+35, 360), "Last", font=dateText, fill=0, align='left')
+        drawRed.text((indent("Run",dateText,w)+35, 400), "Run", font=dateText, fill=0, align='left')
+        drawRed.text((indent(runDate,dateText,w)+35, 440), runDate, font=dateText, fill=0, align='left')
+    
+        distanceInt = round((lastRunJson[0]['distance'] * 0.621), 2)
+        distance = str(distanceInt)
+        duration = lastRunJson[0]['duration']
+        timeMin = str(int(duration / 60))
+        timeSec = str(int(duration % 60))
+        calories = str(lastRunJson[0]['calories'])
+        paceMin = str(int((duration / distanceInt) / 60)) 
+        paceSec = str(int((duration / distanceInt) % 60)).zfill(2)
+        data1=distance+"mi"
+        label1="Dist"
+        data2=timeMin+":"+timeSec
+        label2="Time"
+        data3=paceMin+":"+paceSec
+        label3="Pace"
+        data4=calories
+        label4="Cals"   
+    else:
+        drawRed.text((indent("Stats:",dateText,w)+35, 360), "Stats:", font=dateText, fill=0, align='left')
+        drawRed.text((indent(monthText,dateText,w)+35, 400), monthText, font=dateText, fill=0, align='left')
+        drawRed.text((indent(year,dateText,w)+35, 440), year, font=dateText, fill=0, align='left')
+        totalDist = str(round((monthRunJson['totalDistance']*.621),2))
+        runCount = str(monthRunJson['runCount'])
+        if monthRunJson['longestRun'] == None:
+            longRun = '0'
+        else:
+            longRun = str(round((monthRunJson['longestRun'] * 0.621),2))
+        if monthRunJson['averageRunLength'] == None:
+            avgLen = '0'
+        else:
+            avgLen = str(round((monthRunJson['averageRunLength'] * 0.621),2))
+        data1=runCount+" runs"
+        label1="Cnt"
+        data2=totalDist+"mi"
+        label2="Dist"
+        data3=longRun+"mi"
+        label3="Long"
+        data4=avgLen+"mi"
+        label4="Avg"
 
 
-###SUNRISE TEXT###
-    drawRed.text((indent(sunriseText,bodyText,w/2)+400,0),sunriseText,font=bodyText,fill=0,align='left')
-    drawBlack.text((indent(sunriseFull,sunText,w/2)+400,20),sunriseFull, font=sunText, fill=0, align='left')
-
-###SUNSET TEXT###
-    drawRed.text((indent(sunsetText,bodyText,w/2)+400,80), sunsetText,font=bodyText,fill=0,align='left')
-    drawBlack.text((indent(sunsetFull,sunText,w/2)+400,100),sunsetFull, font=sunText, fill=0, align='left')
-
+    runHorPos=430
+    labelHorPos=375
+    runDataPrint(data1,runHorPos,160)
+    printVert(label1,170,labelHorPos)
+    runDataPrint(data2,runHorPos,240)
+    printVert(label2,240,labelHorPos)
+    runDataPrint(data3,runHorPos,320)
+    printVert(label3,320,labelHorPos)
+    runDataPrint(data4,runHorPos,400)
+    printVert(label4,410,labelHorPos)
 
 
     epd.display(epd.getbuffer(imageBlack),epd.getbuffer(imageRed))
